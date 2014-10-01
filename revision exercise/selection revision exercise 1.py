7a1de8cd69b2f70326dde16877cd02c671c39389
VinceSevilleno <44963@longroad.ac.uk>
2014-10-01 15:09:43 +0100

    selection revision exercise 1 and 2

---
+++ revision exercise/selection revision exercise 1.py
@@ -0,0 +1,13 @@
+#Vince Sevilleno
+#1/10/2014
+#Selection Revision Exercise
+
+integer=int(input("Please enter an integer:"))
+print()
+integer2=int(input("Please enter a second integer:"))
+print()
+
+if integer==integer2:
+    print("The two numbers entered are the same.")
+else:
+    print("The two numbers are NOT the same.")
---
+++ revision exercise/selection revision exercise 2.py
@@ -0,0 +1,11 @@
+#Vince Sevilleno
+#1/10/2014
+#Selection Revision Exercise 2
+
+age=int(input("Please enter your age:"))
+print()
+if age>=17:
+    print("You are old enough to drive.")
+elif age<17:
+    print("You are not old enough to drive.")
+print()
