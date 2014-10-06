17d4a2a57fcc812c15f1e7ff2c6132b2c211b0a2
VinceSevilleno <44963@longroad.ac.uk>
2014-10-06 09:29:03 +0100

    selection revision exercise 3 and 4

---
+++ revision exercise/selection revision exercise 3.py
@@ -0,0 +1,12 @@
+#Vince Sevilleno
+#3/10/2014
+#Selection Revision Exercise 3
+
+number= int(input("Please enter a number between 21-29:"))
+print()
+if number<21:
+    print("This number is too low, please enter a higher number.")
+elif number>29:
+    print("This number is too high, please enter a lower number.")
+else:
+    print("This number is within the range.")
