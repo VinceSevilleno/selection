1b0c062fc8fa4e4681db75d36bcbfa72cde90e8f
VinceSevilleno <44963@longroad.ac.uk>
2014-10-08 10:53:45 +0100

    development exercise 1 selection

---
+++ development exercise/development exercises 1.py
@@ -0,0 +1,35 @@
+#Vince Sevilleno
+#6/10/2014
+#Selection Development Exercise 1
+
+print("This program will associate a number between 1-12 with the 12 months of the year.")
+print()
+month=int(input("Please enter a number between 1-12:"))
+print()
+if month==1:
+    print("You have chosen January.")
+elif month==2:
+    print("You have chosen February.")
+elif month==3:
+    print("You have chosen March.")
+elif month==4:
+    print("You have chosen April.")
+elif month==5:
+    print("You have chosen May.")
+elif month==6:
+    print("You have chosen June.")
+elif month==7:
+    print("You have chosen July.")
+elif month==8:
+    print("You have chosen August.")
+elif month==9:
+    print("You have chosen September.")
+elif month==10:
+    print("You have chosen October.")
+elif month==11:
+    print("You have chosen November.")
+elif month==12:
+    print("You have chosen December.")
+else:
+    print("This number is invalid, please enter a number between 1-12.")
+
